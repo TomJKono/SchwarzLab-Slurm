32f483711019bfd350384ee38b51dfac5fe40f86
Thomas Kono <tom.kono@iccb-cologne.org>
2024-11-30 21:25:08 +0100

    Rename example number

--- task_arrays/task_array_example_5.sh
+++ task_arrays/task_array_example_5.sh
@@ -9,7 +9,7 @@
 #SBATCH -e array_%A-%a.err
 #SBATCH -o array_%A-%a.out
 
-# Example 4: Really, a job array just exports a single integer into the job
+# Example 5: Really, a job array just exports a single integer into the job
 # environment. How you use this integer is up to you!
 NCLUST="${SLURM_ARRAY_TASK_ID}"
 N_REPS=10
